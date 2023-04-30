e14fbb3d3468ce9b5e4da4e5960fc0465a543100
Pritam Singh Shaktawat <shaktawatpritam@gmail.com>
2023-04-30 16:51:47 +0530

    main_activity.java updated

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView weatherRV;
     private TextInputEditText cityEdt;
     private ImageView backIV, iconIV, searchIV;
-
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_main);
         homeRL = findViewById(R.id.idRLHome);
         loadingPB = findViewById(R.id.idPBLoading);
@@ -37,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
         backIV = findViewById(R.id.idIVBack);
         iconIV = findViewById(R.id.idIVIcon);
         searchIV = findViewById(R.id.idIVSearch);
+        weatherRVModalArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this,weatherRVModalArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
 
+    }
+
+    private void getWeatherInfo( String cityName){
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=0bbffba1f423474d9ab112410232204&q="+cityName +"&days=1&aqi=no&alerts=no"
     }
 }
